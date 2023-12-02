01355ac149b2292b520c242ac22c211b8e0f4ee0
Heinen1 <jurnheinen111@gmail.com>
2023-12-02 16:40:51 +0100

    minor changes to readme and main.py

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # degiro_analyser
-Analyse investment portfolio from DeGiro (Dutch broker)
+De Giro is a popular Dutch investment broker offering a wide range of securities. The account export generates .xls, or .csv files containing the various transactions over time, which can be analysed with this code. Presently, only stocks and ETFs are considered.
+
+# Berekeningen
+Onderstaande overzicht weergeeft hoe grootheden berekend worden in de overzichten.
+ - AANTAL = Totaal aantal gekochte shares
+ - GAK = AANTAL / Total gekocht (EUR)
+ - WAARDE = AANTAL * GAK
+ - Uitbetaalde dividend = (DIVIDEND - DEGIRO Corporate Action Kosten) / Valuta Debitering
+ 
+Het overzicht is gesorteerd op de Datum. De Corporate Action Kosten, die bebehoren bij de Dividend, komen overeen met de Valutadatum. Voor een custody-account zijn de kosten voor dividend verwerking: 1.00E + 3.00% van het dividend. Voor VWRL moeten de kosten eerst naar dollars worden omgezet. Hierbij behoort de wisselkoers (FX = Foreign eXchange) van de Valutadatum van de Dividend. Let op: voor 2022 Q1 en Q2
+
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from src.functions import get_amount_and_value_from_description
 from src.functions import read_account_overview
-from src.functions import get_share_price_history
 from src.plots import plot_dividend, plot_transactions
 
 %load_ext autoreload
@@ -10,32 +9,37 @@ from src.plots import plot_dividend, plot_transactions
 # Load account xls into dataframe
 df_account = read_account_overview('Account.xls')
 
-# Define product (does not need to be the entire name)
-product = 'VANGUARD'
+# Define products (does not need to be the entire name)
+products = ['VANGUARD']
 
-# Get only records of pre-defined product
-df_account = df_account[df_account['Product'].str.contains(product)]
+for product in products:
+    # Get only records of pre-defined product
+    df_account = df_account[df_account['Product'].str.contains(product)]
 
-# Select of all transaction of pre-defined product
-transactions = df_account[df_account['Omschrijving'].str.contains('Koop')]
-transactions.sort_values(by=['Datum'], inplace=True)
-transactions[['Transaction_Quantity', 'Transaction_Amount']] = transactions['Omschrijving'].apply(lambda x: get_amount_and_value_from_description(x)).tolist()
-transactions['Transaction_Quantity_cumulative'] = transactions['Transaction_Quantity'].cumsum(axis=0)
-transactions['Transaction_Total'] = transactions['Transaction_Quantity'] * transactions['Transaction_Amount']
-transactions['Transaction_Total_Cum'] = transactions['Transaction_Total'].cumsum(axis=0)
+    # Selection of all transaction of product
+    transactions = df_account[df_account['Omschrijving'].str.contains('Koop')]
+    transactions.sort_values(by=['Datum'], inplace=True)
+    transactions[['Transaction_Quantity', 'Transaction_Amount']] = transactions['Omschrijving'].apply(lambda x: get_amount_and_value_from_description(x)).tolist()
+    transactions['Transaction_Quantity_cumulative'] = transactions['Transaction_Quantity'].cumsum(axis=0)
+    transactions['Transaction_Total'] = transactions['Transaction_Quantity'] * transactions['Transaction_Amount']
+    transactions['Transaction_Total_Cum'] = transactions['Transaction_Total'].cumsum(axis=0)
 
-currency = transactions['Mutatie_Valuta'].values[0]
-plot_transactions(transactions['Datum'],
-                  transactions['Transaction_Total'],
-                  transactions['Transaction_Total_Cum'],
-                  currency)
+    currency = transactions['Mutatie_Valuta'].values[0]
+    plot_transactions(transactions['Datum'],
+                    transactions['Transaction_Total'],
+                    transactions['Transaction_Total_Cum'],
+                    currency)
 
-dividend = df_account[df_account['Omschrijving'].str.contains('Dividend')][['Datum', 'Datum_Year', 'Mutatie_Valuta', 'Mutatie_Bedrag']]
-dividend.sort_values(by=['Datum'], inplace=True)
-dividend['Mutatie_Bedrag_Cum'] = dividend['Mutatie_Bedrag'].cumsum(axis=0)
+    # This is not the eactual received divident on the Flatex bank account.
+    # One must subtract the DEGIRO Corporate Action Kosten as well as any foreign exchange conversion
+    dividend = df_account[df_account['Omschrijving'].str.contains('Dividend')][['Datum', 'Datum_Year', 'Mutatie_Valuta', 'Mutatie_Bedrag']]
+    dividend.sort_values(by=['Datum'], inplace=True)
+    dividend['Mutatie_Bedrag_Cum'] = dividend['Mutatie_Bedrag'].cumsum(axis=0)
 
-currency = dividend['Mutatie_Valuta'].values[0]
-plot_dividend(dividend['Datum'],
-              dividend['Mutatie_Bedrag'],
-              dividend['Mutatie_Bedrag_Cum'],
-              currency)
+    currency = dividend['Mutatie_Valuta'].values[0]
+    plot_dividend(dividend['Datum'],
+                dividend['Mutatie_Bedrag'],
+                dividend['Mutatie_Bedrag_Cum'],
+                currency)
+    
+    dividend.groupby('Datum_Year')['Mutatie_Bedrag'].describe()
